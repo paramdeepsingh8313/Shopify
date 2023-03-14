e47c9b6f4dcbd5d9dfb5c16909988cff8d751140
paramdeepsingh8313 <roshansingh8313@gmail.com>
2023-03-14 23:08:56 +0530

    added redux to the app

--- src/components/body/Body.js
+++ src/components/body/Body.js
@@ -1,12 +1,16 @@
 import React from "react";
 import Card from "../card/Card";
 import "./Body.css";
+import { useStateValue } from "../../redux/StateProvider";
 
 function Body(props) {
+  const [{ name, age }, dispatch] = useStateValue();
   return (
     <>
       <div className="body">
         {props.data}
+        {name && name}
+        {age && age}
         <Card />
         <Card />
         <Card />
