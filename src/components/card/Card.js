02aa380eb62176c3d8f13bc1391eca256cd1a500
paramdeepsingh8313 <roshansingh8313@gmail.com>
2023-03-20 23:37:00 +0530

    wrote the code for wishlist and cartlist

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -3,8 +3,33 @@ import "./Card.css";
 
 import cart from "../../assets/icons/cart.svg";
 import wishlist from "../../assets/icons/wishlist.svg";
+import { useStateValue } from "../../redux/StateProvider";
 
 function Card(props) {
+  const [{ WishlistArray, cartArray }, dispatch] = useStateValue();
+
+  console.log("cartArray", cartArray);
+
+  function checkWishList(id) {
+    if (WishlistArray.includes(id)) {
+    } else {
+      dispatch({
+        type: "WISHLIST",
+        value: id,
+      });
+    }
+  }
+
+  function checkCartList(id) {
+    if (cartArray.includes(id)) {
+    } else {
+      dispatch({
+        type: "CARTLIST",
+        value: id,
+      });
+    }
+  }
+
   return (
     <div>
       <div className="card">
@@ -28,47 +53,22 @@ function Card(props) {
               src={require(`../../assets/images/home&living/${props?.data?.img}`)}
             />
           )}
-
-          {/* {props.data && props?.data?.categoryId === 1001 ? (
-            <img
-              src={require(`../../assets/images/men/${
-                props.data && props?.data?.img
-              }`)}
-            />
-          ) : props.data && props?.data?.categoryId === 1002 ? (
-            <img
-              src={require(`../../assets/images/women/${
-                props.data && props?.data?.img
-              }`)}
-            />
-          ) : props.data && props?.data?.categoryId === 1003 ? (
-            <img
-              src={require(`../../assets/images/kids/${
-                props.data && props?.data?.img
-              }`)}
-            />
-          ) : props.data && props?.data?.categoryId === 1004 ? (
-            <img
-              src={require(`../../assets/images/beauty/${
-                props.data && props?.data?.img
-              }`)}
-            />
-          ) : (
-            <img
-              src={require(`../../assets/images/home&living/${
-                props.data && props?.data?.img
-              }`)}
-            />
-          )} */}
         </div>
         <div className="cardDesc">
           <p>{props.data && props?.data?.brand}</p>
         </div>
         <div className="cardBtnDiv">
-          <button className="cardBtn">
+          <button
+            className="cardBtn"
+            onClick={() => checkWishList(props.data && props?.data?.itemID)}
+          >
             <img src={wishlist} />
           </button>
-          <button className="cardBtn">
+
+          <button
+            className="cardBtn"
+            onClick={() => checkCartList(props.data && props?.data?.itemID)}
+          >
             <img src={cart} />
           </button>
         </div>
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,5 +1,7 @@
 export const initialState = {
   apiData: [],
+  WishlistArray: [11000001],
+  cartArray: [],
   name: "Roshan",
   age: 20,
 };
@@ -8,11 +10,25 @@ export const actionTypes = {
   APIDATA: "APIDATA",
   NAME: "NAME",
   AGE: "AGE",
+  WISHLIST: "WISHLIST",
+  CARTLIST: "CARTLIST",
 };
 
 export const reducer = (state, action) => {
   switch (action.type) {
     case "APIDATA":
       return { ...state, apiData: action.value };
+
+    case "WISHLIST":
+      return {
+        ...state,
+        WishlistArray: [...state.WishlistArray, action.value],
+      };
+
+    case "CARTLIST":
+      return {
+        ...state,
+        cartArray: [...state.cartArray, action.value],
+      };
   }
 };
